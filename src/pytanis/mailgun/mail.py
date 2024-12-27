53313c56af13ed86054b75c04ee0cd7b3940cbb4
Nils Finke <mail@nils-finke.com>
2024-12-27 13:30:18 +0000

    Mailgun send mail implementation

--- src/pytanis/mailgun/mail.py
+++ src/pytanis/mailgun/mail.py
@@ -1,7 +1,10 @@
 import time
 
+import requests
 from pydantic import BaseModel, ConfigDict, validator
 from tqdm.auto import tqdm
+
+from pytanis.config import Config, get_cfg
 
 
 class MetaData(BaseModel):
@@ -62,23 +65,53 @@ class MailClient:
 
     batch_size: int = 10  # n messages are a batch
     wait_time: int = 20  # wait time after eacht batch before next
+    timeout: int = 10  # timeout for requests in seconds
 
-    def __init__(self):
-        # TODO: add instantiation for Mailclient?
-        pass
+    def __init__(self, config: Config | None = None):
+        if config is None:
+            config = get_cfg()
+        self._config = config
 
     # TODO: Check return type of mail
     def send(self, mail: Mail):
         """Send a mail to all recipients using Mailgun"""
         errors = []
-        status_msg = None
+        responses = []
+
+        # TODO: improve Mailgun batch mailing by setting custom transactional variables
         for idx, recipient in enumerate(tqdm(mail.recipients), start=1):
             try:
-                pass
+                recipient_mail = mail.model_copy()
+                if self._config.Mailgun.token is None:
+                    msg = 'API token for Mailgun is empty'
+                    raise RuntimeError(msg)
+                if self._config.Mailgun.from_address is None:
+                    msg = 'From Email for Mailgun is empty'
+                    raise RuntimeError(msg)
+                if self._config.Mailgun.reply_to is None:
+                    msg = 'Reply To Email for Mailgun is empty'
+                    raise RuntimeError(msg)
+
+                response = requests.post(
+                    'https://api.eu.mailgun.net/v3/mg.pycon.de/messages',
+                    auth=('api', self._config.Mailgun.token),
+                    data={
+                        'to': [recipient.email],
+                        'from': self._config.Mailgun.from_address,
+                        'subject': recipient_mail.subject.format(recipient=recipient, mail=mail),
+                        'text': recipient_mail.body.format(recipient=recipient, mail=mail),
+                        'h:Reply-To': self._config.Mailgun.reply_to,
+                    },
+                    timeout=self.timeout,
+                )
+                # check response status message and throw exception if not 200
+                response.raise_for_status()
             except Exception as e:
                 errors.append((recipient, e))
+            else:
+                responses.append(response)
 
             if idx % self.batch_size == 0:
                 time.sleep(self.wait_time)
 
-        return status_msg, errors
+        return responses, errors
