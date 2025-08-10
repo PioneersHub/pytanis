9c12b9403b6e53947e5938c6e32fe53693ba0321
Alexander CS Hendorf <alexander@pioneershub.org>
2025-08-10 18:02:26 +0200

    fix: version

--- src/pytanis/__init__.py
+++ src/pytanis/__init__.py
@@ -1,6 +1,6 @@
 """PyTanis - Python client for Pretalx."""
 
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 from importlib.metadata import PackageNotFoundError, version
 from typing import TYPE_CHECKING
